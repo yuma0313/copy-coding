da9a1e29cf15dc49f4433f5c70be3e7db1634997
yuma0313 <y.yuma0313@gmail.com>
2023-04-18 19:54:54 +0900

    中級編デスクトップ画面作成

---
+++ 中級編/index.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburgerMenu.classList.toggle("open");
+  navMenu.classList.toggle("open");
+}
