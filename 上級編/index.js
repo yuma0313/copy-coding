fffde7df0e0713187a06f55c51c9b957af22f83b
yuma0313 <y.yuma0313@gmail.com>
2023-04-28 17:53:32 +0900

    advanced coding:desktop

--- 上級編/index.js
+++ 上級編/index.js
@@ -1,3 +1,4 @@
+//ハンバーガーメニュー表示のJS
 function toggleMenu() {
   const hamburgerMenu = document.querySelector(".hamburger-menu");
   const navMenu = document.querySelector(".nav-menu");
@@ -7,3 +8,30 @@ function toggleMenu() {
   navMenu.classList.toggle("open");
   mask.classList.toggle("open");
 }
+
+$(document).ready(function () {
+  $(".carousel").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
